daffe49ee71837e08fc9d7d64ab830cd3159ecc5
NandoAntonio <fernandoant987@hotmail.com>
2026-07-14 22:13:15 -0300

    Atualizando o código Js do projeto

--- github-markdown/exemplo_versionamento.js
+++ github-markdown/exemplo_versionamento.js
@@ -14,7 +14,7 @@ function soma(numA, numB){
 
 console.log("") // Pular linha
 
-let userName = getFirstName("Camila-Reis-Correa", "-") //Com o splitChar na chamada da funcao passar o valor que vai separar as strings
+let userName = getFirstName("Fernando-Antonio-Costa", "-") //Com o splitChar na chamada da funcao passar o valor que vai separar as strings
 console.log(`Seja Bem-vindo ao meu estabelecimento: ${userName}`)
 
 userName = getFirstName("Fernando da Costa Antonio", " ")
